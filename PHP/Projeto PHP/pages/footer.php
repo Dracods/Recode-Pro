43587ca3cc3699b2c31a801d825ad7d177902d8d
Dracods <39202981+Dracods@users.noreply.github.com>
2020-10-27 12:01:36 -0300

    MySQL e PHP adicionados

---
+++ PHP/Projeto PHP/pages/footer.php
@@ -0,0 +1,17 @@
+<footer>
+    <div class="footerMisc">
+        <div class="formasPagamento">
+            <p>Formas de pagamento</p><img src="imagens/icones/bandeiras_cartoes.png">
+        </div>
+        <div>
+            Ícones feitos por <a href="https://www.flaticon.com/br/autores/freepik" target="_blank"
+                title="Freepik">Freepik</a> do
+            <a href="https://www.flaticon.com/br/" target="_blank" title="Flaticon">Flaticon</a>
+        </div>
+    </div>
+    <div class="assinatura">
+        <a href="https://github.com/Vinicius-Brito-Costa" target="_blank" title="Perfil do Github">
+            <p>Desenvolvido por Vinicius Brito Costa</p>
+        </a>
+    </div>
+</footer>
---
+++ PHP/Projeto PHP/pages/header.php
@@ -0,0 +1,16 @@
+<header>
+    <div class="menu-principal">
+        <a href="index.php">
+            <div class="headerHeight"></div><img id="logo" src="imagens/fs-logo.png">
+        </a>
+        <a href="products.php" id="paginas" class="linkPagina"><img src="imagens/icones/iconProdutos.svg" class="icones">
+            <p>PRODUTOS</p>
+        </a>
+        <a href="contact.php" id="paginas" class="linkPagina"><img src="imagens/icones/iconContato.svg" class="icones">
+            <p>CONTATO</p>
+        </a>
+        <a href="about.php" id="paginas" class="linkPagina"><img src="imagens/icones/iconSobre.svg" class="icones">
+            <p>SOBRE</p>
+        </a>
+    </div class="menu-principal">
+</header>
